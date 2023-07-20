386c500c0e8882c770a4ee39f7331dcf2d7fe3de
RuslanGafurov <g.r.9@mail.ru>
2023-07-20 10:00:28 +0300

    Added cities_list_view

--- cities/views.py
+++ cities/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+from cities.models import City
+
+__all__ = (
+    'cities_list_view',
+)
+
+
+def cities_list_view(request):
+    query_set = City.objects.all()
+    context = {'objects_list': query_set}
+    return render(request, 'cities/list.html', context)
