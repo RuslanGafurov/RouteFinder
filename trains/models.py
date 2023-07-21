85d11ca71a0d186a4d45697adcadf49ecd8eff2c
RuslanGafurov <g.r.9@mail.ru>
2023-07-21 15:28:18 +0300

    Added Train model

--- trains/models.py
+++ trains/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class Train(models.Model):
+    name = models.CharField(
+        max_length=50,
+        unique=True,
+        verbose_name='Поезд',
+    )
+    travel_time = models.PositiveSmallIntegerField(
+        verbose_name='Время в пути'
+    )
+    from_city = models.ForeignKey(
+        'cities.City',
+        on_delete=models.CASCADE,
+        related_name='from_city_set',
+        verbose_name='Из какого города'
+    )
+    to_city = models.ForeignKey(
+        'cities.City',
+        on_delete=models.CASCADE,
+        related_name='to_city_set',
+        verbose_name='В какой город'
+    )
+    
+    def __str__(self):
+        return f"Поезд {self.name} из {self.from_city} в {self.to_city}"
+    
+    class Meta:
+        verbose_name = 'поезд'
+        verbose_name_plural = 'поезда'
+        ordering = ['travel_time']
