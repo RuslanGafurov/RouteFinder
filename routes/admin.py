5301420843d136c2c4386b6d8fb5f651a009f8c1
RuslanGafurov <g.r.9@mail.ru>
2023-07-22 13:25:07 +0300

    Added RouteAdmin

--- routes/admin.py
+++ routes/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from routes.models import Route
 
-# Register your models here.
+
+class RouteAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Route
+
+    list_display = ('name', 'from_city', 'to_city', 'route_travel_time')
+    list_editable = ('route_travel_time',)
+
+
+admin.site.register(Route, RouteAdmin)
