3d7b9535fb81cee31171fe33b30a93f8d193fb41
RuslanGafurov <g.r.9@mail.ru>
2023-07-22 13:05:42 +0300

    Added routes app

--- route_finder/urls.py
+++ route_finder/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('cities/', include(('cities.urls', 'cities'))),
     path('trains/', include(('trains.urls', 'trains'))),
+    path('routes/', include(('routes.urls', 'routes'))),
 ]
