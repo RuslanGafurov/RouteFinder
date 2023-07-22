e37c9af28403d62a4259a36b7ce6a56762072c7a
RuslanGafurov <g.r.9@mail.ru>
2023-07-22 13:40:30 +0300

    Added RouteListView

--- routes/views.py
+++ routes/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
+from django.views.generic import DetailView, DeleteView, UpdateView, ListView, CreateView
 
-# Create your views here.
+from routes.forms import RouteForm
+from routes.models import Route
+
+
+class RouteListView(ListView):
+    model = Route
+    template_name = 'home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(RouteListView, self).get_context_data(**kwargs)
+        context['form'] = RouteForm()
+        return context
