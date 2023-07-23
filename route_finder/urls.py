3abcd8e55a50cf1f1ced4542d1b23874edb8373a
RuslanGafurov <g.r.9@mail.ru>
2023-07-23 15:56:48 +0300

    Changed RouteListView to home_view

--- route_finder/urls.py
+++ route_finder/urls.py
@@ -17,11 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from routes.views import RouteListView, route_search_view
+from routes.views import home_view, route_search_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', RouteListView.as_view(), name='home'),
+    path('', home_view, name='home'),
     path('route_search/', route_search_view, name='route_search'),
     path('cities/', include(('cities.urls', 'cities'))),
     path('trains/', include(('trains.urls', 'trains'))),
--- routes/views.py
+++ routes/views.py
@@ -1,10 +1,13 @@
 from django.contrib import messages
 from django.shortcuts import render
-from django.views.generic import ListView
 
 from routes.forms import RouteForm
-from routes.models import Route
 from routes.services import get_routes
+
+
+def home_view(request):
+    form = RouteForm()
+    return render(request, 'home.html', {'form': form})
 
 
 def route_search_view(request):
@@ -12,7 +15,7 @@ def route_search_view(request):
         form = RouteForm(request.POST)
         if form.is_valid():
             try:
-                context = get_routes(request, form)
+                context = get_routes(form)
             except ValueError as err:
                 messages.error(request, err)
                 return render(request, 'home.html', {'form': form})
@@ -23,13 +26,3 @@ def route_search_view(request):
         form = RouteForm()
         messages.error(request, 'Нет данных для поиска')
         return render(request, 'home.html', {'form': form})
-
-
-class RouteListView(ListView):
-    model = Route
-    template_name = 'home.html'
-
-    def get_context_data(self, **kwargs):
-        context = super(RouteListView, self).get_context_data(**kwargs)
-        context['form'] = RouteForm()
-        return context
