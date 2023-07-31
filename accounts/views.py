028edf1229a55e15690e9b09b257369450da5fb2
RuslanGafurov <g.r.9@mail.ru>
2023-07-31 14:28:51 +0300

    Remade views from FBV to CBV and added UserProfileView

--- accounts/views.py
+++ accounts/views.py
@@ -1,38 +1,31 @@
-from django.contrib.auth import authenticate, login, logout
-from django.shortcuts import redirect, render
+from django.contrib.auth.views import LoginView
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, UpdateView
 
-from accounts.forms import UserLoginForm, UserRegistrationForm
-
-
-def login_view(request):
-    form = UserLoginForm(request.POST or None)
-    _next = request.GET.get('next')
-    if form.is_valid():
-        username = form.cleaned_data.get('username')
-        password = form.cleaned_data.get('password')
-        user = authenticate(username=username, password=password)
-        login(request, user)
-        _next = _next or '/'
-        return redirect(_next)
-    else:
-        return render(request, 'accounts/login.html', {'form': form})
+from accounts.forms import UserLoginForm, UserProfileForm, UserRegistrationForm
+from accounts.models import UserModel
 
 
-def logout_view(request):
-    logout(request)
-    return redirect('/')
+class UserLoginView(SuccessMessageMixin, LoginView):
+    template_name = 'accounts/login.html'
+    form_class = UserLoginForm
+    success_message = 'Вы успешно вошли в аккаунт'
 
 
-def registration_view(request):
-    if request.method == 'POST':
-        form = UserRegistrationForm(request.POST)
-        if form.is_valid():
-            new_user = form.save(commit=False)
-            new_user.set_password(form.cleaned_data['password1'])
-            new_user.save()
-            return render(request, 'accounts/register_done.html', {'new_user': new_user})
-        else:
-            return render(request, 'accounts/register.html', {'form': form})
-    else:
-        form = UserRegistrationForm()
-        return render(request, 'accounts/register.html', {'form': form})
+class UserRegistrationView(SuccessMessageMixin, CreateView):
+    model = UserModel
+    form_class = UserRegistrationForm
+    template_name = 'accounts/registration.html'
+    success_url = reverse_lazy('accounts:login')
+    success_message = 'Регистрация прошла успешно! Теперь Вы можете войти'
+
+
+class UserProfileView(SuccessMessageMixin, UpdateView):
+    model = UserModel
+    form_class = UserProfileForm
+    template_name = 'accounts/profile.html'
+    success_message = 'Данные успешно изменены'
+
+    def get_success_url(self):
+        return reverse_lazy('accounts:profile', kwargs={'pk': self.object.pk})
