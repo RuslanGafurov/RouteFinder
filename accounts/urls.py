3ef2af31eaa4367f8bc1e477bf7a907204242744
RuslanGafurov <g.r.9@mail.ru>
2023-07-31 14:29:36 +0300

    Remade urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,11 @@
+from django.contrib.auth.views import LogoutView
 from django.urls import path
 
-from accounts.views import login_view, logout_view, registration_view
+from accounts.views import UserLoginView, UserProfileView, UserRegistrationView
 
 urlpatterns = [
-    path('login/', login_view, name='login'),
-    path('logout/', logout_view, name='logout'),
-    path('register/', registration_view, name='register'),
+    path('login/', UserLoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('registration/', UserRegistrationView.as_view(), name='registration'),
+    path('profile/<int:pk>', UserProfileView.as_view(), name='profile'),
 ]
