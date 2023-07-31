f99876a31a31b4697e8f36c36f0c9de53524d5d0
RuslanGafurov <g.r.9@mail.ru>
2023-07-31 14:27:26 +0300

    Added tests for app accounts

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,48 @@
-from django.test import TestCase
+from http import HTTPStatus
 
-# Create your tests here.
+from django.test import TestCase
+from django.urls import reverse
+
+from accounts.models import UserModel
+from common.tests import common_tests
+
+
+class UserRegistrationTest(TestCase):
+
+    def setUp(self) -> None:
+        self.path = reverse('accounts:registration')
+        self.data = {
+            'username': 'Test',
+            'password1': 'asFFcv68g',
+            'password2': 'asFFcv68g',
+        }
+
+    def test_user_registration_get(self):
+        response = self.client.get(self.path)
+
+        common_tests(self, response, 'accounts/registration.html')
+
+    def test_user_registration_post_success(self):
+        username = self.data['username']
+        self.assertFalse(UserModel.objects.filter(username=username).exists())
+        response = self.client.post(self.path, self.data)
+
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+        self.assertRedirects(response, reverse('accounts:login'))
+        self.assertTrue(UserModel.objects.filter(username=username).exists())
+
+    def test_user_registration_post_error(self):
+        UserModel.objects.create(username=self.data['username'])
+        response = self.client.post(self.path, self.data)
+
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertContains(response, 'Пользователь с таким именем уже существует.', html=True)
+
+
+class UserLoginTest(TestCase):
+
+    def test_user_login(self):
+        path = reverse('accounts:login')
+        response = self.client.get(path)
+
+        common_tests(self, response, 'accounts/login.html')
